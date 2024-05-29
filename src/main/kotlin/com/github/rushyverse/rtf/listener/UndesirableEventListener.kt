1a769a41a22393374b05cea450b4abf9f0274042
Cizetux <cizetux@gmail.com>
2024-05-29 15:53:47 +0200

    feat: add new unwanted events

--- src/main/kotlin/com/github/rushyverse/rtf/listener/UndesirableEventListener.kt
+++ src/main/kotlin/com/github/rushyverse/rtf/listener/UndesirableEventListener.kt
@@ -2,14 +2,18 @@ package com.github.rushyverse.rtf.listener
 
 import com.github.rushyverse.api.extension.event.cancelIf
 import org.bukkit.event.EventHandler
+import org.bukkit.event.entity.EntityDamageEvent
 import org.bukkit.event.entity.FoodLevelChangeEvent
+import org.bukkit.event.entity.PlayerDeathEvent
 import org.bukkit.event.player.PlayerDropItemEvent
 import org.bukkit.event.weather.WeatherChangeEvent
 
 class UndesirableEventListener : ListenerRTF() {
 
     @EventHandler
-    fun onFoodLevelChange(event: FoodLevelChangeEvent) = event.cancelIf { isRTFWorld(event.entity.world) }
+    fun onFoodLevelChange(event: FoodLevelChangeEvent) = event.cancelIf {
+        isRTFWorld(event.entity.world)
+    }
 
     @EventHandler
     fun onWeatherChange(event: WeatherChangeEvent) = event.cancelIf {
@@ -17,5 +21,15 @@ class UndesirableEventListener : ListenerRTF() {
     }
 
     @EventHandler
-    fun onPlayerDropItem(event: PlayerDropItemEvent) = event.cancelIf { isRTFWorld(event.player.world) }
+    fun onPlayerDropItem(event: PlayerDropItemEvent) = event.cancelIf {
+        isRTFWorld(event.player.world)
+    }
+
+    @EventHandler
+    fun onPlayerDeath(event: PlayerDeathEvent) {
+        val player = event.entity
+        if (isRTFWorld(player.world) && player.lastDamageCause?.cause == EntityDamageEvent.DamageCause.FALL) {
+            event.drops.clear()
+        }
+    }
 }
